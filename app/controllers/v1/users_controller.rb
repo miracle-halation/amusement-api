0ae34b42e9f6fd0fafd5d372f4d5ebc18df84bec
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-27 08:57:05 +0900

    uidでログインしているユーザーを見つける

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class V1::UsersController < ApplicationController
 	def index
-		current_user = User.find(params[:uid])
-		render json: current_user
+		@user = User.find_by(uid: params[:uid])
+		render json: @user
 	end
 end
