ac3f53cd224f788fef726c1f470b7698a3309e8a
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-26 10:25:44 +0900

    corsによる通信を開放

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+		# origins '*'
+		if Rails.env.production?
+			origins ''
+		else
+			origins 'http://localhost:3000'
+		end
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
