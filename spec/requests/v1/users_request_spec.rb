10fe690357d95b0b8c81cafab0204061ed465020
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-28 11:18:37 +0900

    rubocopでコードを修正

--- spec/requests/v1/users_request_spec.rb
+++ spec/requests/v1/users_request_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "V1::Users", type: :request do
-	describe "UserAPI" do
-		let!(:user){FactoryBot.create(:user)}
-		describe "GET #index" do
-			it "uidのクエリパラメータと一致するユーザーを取得する" do
-				uid_query = user.uid
-				get v1_users_path(uid_query)
-				json = JSON.parse(response.body)
-				expect(response.status).to eq(200)
-			end
-		end
-	end
+RSpec.describe 'V1::Users', type: :request do
+  describe 'UserAPI' do
+    let!(:user) {FactoryBot.create(:user)}
+    describe 'GET #index' do
+      it 'uidのクエリパラメータと一致するユーザーを取得する' do
+        uid_query = user.uid
+        get v1_users_path(uid_query)
+        JSON.parse(response.body)
+        expect(response.status).to eq(200)
+      end
+    end
+  end
 end
