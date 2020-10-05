1f997618a06005c2cb1f2fb735c7d48b2edc9621
miracle-halation <miraclehalation.9710@gmail.com>
2020-10-05 21:49:10 +0900

    indexとshowアクションを記載

--- app/controllers/v1/puzzles_controller.rb
+++ app/controllers/v1/puzzles_controller.rb
@@ -1,8 +1,19 @@
 class V1::PuzzlesController < ApplicationController
 	def index
+		shapes = Shape.all
+		render json: shapes
 	end
 
 	def show
+		puzzle = Puzzle.find(params[:id])
+		shape = puzzle.shape
+		render json: [puzzle, shape]
+	end
+
+	def new
+	end
+
+	def create
 	end
 
 	def answer
